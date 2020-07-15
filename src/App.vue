c149929e6d26602fa47cee45cb484ac108c436ff
fan <1191237272@qq.com>
2020-07-15 14:58:01 +0800

    使用mockjs

--- src/App.vue
+++ src/App.vue
@@ -11,9 +11,20 @@ export default {
   },
   data(){
     return {
-
+      res:{}
     }
   },
+  mounted(){
+    // 本地加载请求静态json文件的形式
+    // this.axios.get('/mock/user/login.json').then((res)=>{
+    //   this.res = res;
+    // });
+    // 本地集成mockjs实现数据mock
+    this.axios.get('/user/login').then((res)=>{
+      this.res = res;
+      console.log(this.res)
+    });
+  }
 }
 </script>
 
