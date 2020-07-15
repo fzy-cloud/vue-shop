c032c221e090f635070938fd93aa9bc2aeb6cd1d
fan <1191237272@qq.com>
2020-07-15 18:31:16 +0800

    开发顶部导航栏

--- src/App.vue
+++ src/App.vue
@@ -11,30 +11,14 @@ export default {
   },
   data(){
     return {
-      res:{}
+      
     }
   },
   mounted(){
-    // 本地加载请求静态json文件的形式
-    // this.axios.get('/mock/user/login.json').then((res)=>{
-    //   this.res = res;
-    // });
-    // 本地集成mockjs实现数据mock
-    this.axios.get('/user/login').then((res)=>{
-      this.res = res;
-      console.log(this.res)
-    });
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './assets/sass/reset.scss';
 </style>
