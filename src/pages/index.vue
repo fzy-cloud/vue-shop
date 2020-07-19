b9544b5295612b5b95315a45f5a5ba9dc617aa8d
fan <1191237272@qq.com>
2020-07-20 01:25:52 +0800

    手机商品的实现

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -60,8 +60,34 @@
 					<img src="/imgs/banner-1.png" alt="">
 				</a>
 			</div>
-			<div class="product-box"></div>
 		</div>
+		<div class="product-box">
+			<div class="container">
+				<h2>手机</h2>
+				<div class="wrapper">
+					<div class="banner-left">
+						<a href="">
+							<img src="/imgs/mix-alpha.jpg" alt="">
+						</a>
+					</div>
+					<div class="list-box">
+            <div class="list" v-for="(arr,i) in phoneList" v-bind:key="i">
+              <div class="item" v-for="(item,j) in arr" v-bind:key="j">
+                <span v-bind:class="{'new-pro':j%2==0}">新品</span>
+                <div class="item-img">
+                  <img :src="item.mainImage" alt="">
+                </div>
+                <div class="item-info">
+                  <h3>{{item.name}}</h3>
+                  <p>{{item.subtitle}}</p>
+                  <p class="price" @click="addCart(item.id)">{{item.price}}元</p>
+                </div>
+              </div>
+            </div>
+          </div>
+				</div>
+			</div>
+			</div>
 		<service-bar></service-bar>
 	</div>
 </template>
@@ -154,9 +180,25 @@ export default {
 					img:'/imgs/ads/ads-4.jpg'
 				}
 			],
+			phoneList:[],
 		}
 	},
-	
+	mounted(){
+		this.init();
+	},
+	methods:{
+		init(){
+			this.axios.get('/products',{
+				params:{
+					category:100012,
+					pageSize:14
+				}
+			}).then((res)=>{
+				res.list = res.list.slice(6,14);
+				this.phoneList = [res.list.slice(0,4),res.list.slice(4,8)]
+			})
+		}
+	}
 }
 </script>
 <style lang="scss">
@@ -252,5 +294,87 @@ export default {
 		.banner{
 			margin-bottom: 50px;
 		}
+		.product-box{
+      background-color:$colorJ;
+      padding:30px 0 50px;
+      h2{
+        font-size:$fontF;
+        height:21px;
+        line-height:21px;
+        color:$colorB;
+        margin-bottom:20px;
+      }
+      .wrapper{
+        display:flex;
+        .banner-left{
+          margin-right:16px;
+          img{
+            width:224px;
+            height:619px;
+          }
+        }
+        .list-box{
+          .list{
+            @include flex();
+            width:986px;
+            margin-bottom:14px;
+            &:last-child{
+              margin-bottom:0;
+            }
+            .item{
+              width:236px;
+              height:302px;
+              background-color:$colorG;
+              text-align:center;
+              span{
+                display:inline-block;
+                width:67px;
+                height:24px;
+                font-size:14px;
+                line-height:24px;
+                color:$colorG;
+                &.new-pro{
+                  background-color:#7ECF68;
+                }
+                &.kill-pro{
+                  background-color:#E82626;
+                }
+              }
+              .item-img{
+                img{
+                  width:100%;
+                  height:195px;
+                }
+              }
+              .item-info{
+                h3{
+                  font-size:$fontJ;
+                  color:$colorB;
+                  line-height:$fontJ;
+                  font-weight:bold;
+                }
+                p{
+                  color:$colorD;
+                  line-height:13px;
+                  margin:6px auto 13px;
+                }
+                .price{
+                  color:#F20A0A;
+                  font-size:$fontJ;
+                  font-weight:bold;
+                  cursor:pointer;
+                  &:after{
+                    @include bgImg(22px,22px,'/imgs/icon-cart-hover.png');
+                    content:' ';
+                    margin-left:5px;
+                    vertical-align: middle;
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
 	}
 </style>
